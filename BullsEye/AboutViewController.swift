0611e711f421b0f25af95341c2010d89cdb2e797
Austen Allred <austen.allred@gmail.com>
2014-12-08 12:32:00 -0700

    add AboutViewController

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Austen Allred on 12/8/14.
+//  Copyright (c) 2014 Austen Allred. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func close() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
